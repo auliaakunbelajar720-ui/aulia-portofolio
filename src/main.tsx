9ece7f93368314696fc6e3d4e8d47f8ccc8a0a3b
Aulia Fakhrul Mubarraq <auliaakunbelajar720@gmail.com>
2026-05-29 09:50:37 +0700

    fix theme provider crash

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { ThemeProvider } from "./components/theme-provider.tsx";
-
 createRoot(document.getElementById("root")!).render(
-  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-    <App />
-  </ThemeProvider>
+  <App />
 );
